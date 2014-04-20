236ce7625cb6647787497e8f7ebbd2c736dec2f8
gmchale <gerardmchale1@gmail.com>
2014-04-20 20:12:05 +0100

    Add comments for the script

--- hello_world.ps1
+++ hello_world.ps1
@@ -1,3 +1,19 @@
+<#
+.SYNOPSIS
+Print hello world plus the name passed to the script.
+
+.DESCRIPTION
+Print hello world plus the string passed to the script. If no input parameter is received the script will only print "Hello world!".
+
+.PARAMETER Name
+The name that should appear as part of the output.
+
+.INPUT
+[String]
+
+.OUTPUT
+[String]
+#>
 [CmdletBinding()]
 Param
 (
